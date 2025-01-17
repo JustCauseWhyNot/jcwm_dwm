f2911b573b7d2f1f903ca3977544164abfd3b55d
JustCauseWhyNot <JustCauseWhyNot@tuta.io>
2025-01-16 20:28:49 -0700

    Revert "feat: apply swallow patch"
    This reverts commit 6d799f8890aae1146e93e70aad5abc86c3cf39b7.

--- config.h
+++ config.h
@@ -3,8 +3,6 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
-static const int swterminheritfs    = 1;        /* 1 terminal inherits fullscreen on unswallow, 0 otherwise */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[] = {
@@ -52,14 +50,14 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title            tags mask  isfloating  isterminal  noswallow  monitor isgame */
-	{ "Gimp",     NULL,       NULL,            0,         1,          0,           0,        -1 },
-	{ "Firefox",  NULL,       NULL,            1 << 8,    0,          0,          -1,        -1 },
-	{ "st-direct",NULL,       NULL,            0,         0,          1,           0,        -1 },
-       { "pavucontrol-qt", NULL,  NULL,            0,         0,          0,           0,         0 },
-	{ NULL,       NULL,       "Event Tester",  0,         0,          0,           1,        -1 }, /* xev */
-	{ "Steam",    NULL,       NULL,            0,            0,           -1,      0,         0,      1 },
-	{ "steam_app",NULL,       NULL,            0,            0,           -1,      0,         0,      1 },
+	/* class      instance    title           tags mask  isfloating  monitor isgame */
+	{ "Gimp",     NULL,       NULL,           0,         0,          -1 },
+	{ "Firefox",  NULL,       NULL,           1 << 8,    0,          -1 },
+	{ "st-direct",NULL,       NULL,           0,         0,          -1 },
+	{ NULL,       NULL,       "Event Tester", 0,         0,          -1 }, /* xev */
+	{ "Steam",    NULL,       NULL,           0,         0,          -1,     1 },
+	{ "steam_app",NULL,       NULL,           0,         0,          -1,     1 },
+	{ "pavucontrol-qt",NULL,  NULL,           0,         0,          -1 },
 };
 
 /* layout(s) */
